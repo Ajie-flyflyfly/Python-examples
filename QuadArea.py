274cc77e7948043610c25bd964bcf708fe1b7e39
Ajie <2107025567@qq.com>
2023-09-07 14:37:41 +0800

    test

--- QuadArea.py
+++ QuadArea.py
@@ -2,14 +2,14 @@
 # CALCULATING QUADRILATERAL AREA
 
 # accepting values for the heights of the triangles from the user
-h1 = float(input('Enter first height: '))  
-h2 = float(input('Enter second height: '))  
+v1 = float(input('Enter first height: '))  
+v2 = float(input('Enter second height: '))  
 
 # accepting values for the diagonal  from the user
 d = float(input('Enter the diagonal: '))  
 
   
 # calculate the area 
-area = 0.5 * d * (h1+h2) 
+area = 0.5 * d * (v1+v2) 
 
 print("The area of the triangle is %0.2f" %area)   
